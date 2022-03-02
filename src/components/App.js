ef89d69ef670432fc139552def00a50bd5437936
KyleBPerez <star_wars51389@yahoo.com>
2022-03-01 19:26:19 -0800

    App.js set up basic routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,34 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import Header from './Header'
 
 function App() {
+  const [user, setUser] = useState({})
+  const [routines, setRoutines] = useState([])
+  const [activites, setActivities] = useState([])
+  const [token, setToken] = useState('')
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      setToken(localStorage.getItem('token'))
+    }
+  }, [])
+
   return (
-    <Container className='App mw-100 p-0'>
+    <div className='App'>
       <Header />
-    </Container>
+      <div className='content-container d-flex justify-content-center'>
+        <Routes>
+          <Route path='/' element={<h1>Home</h1>} />
+          <Route path='/routines' element={<h1>Routines</h1>} />
+          <Route path='/activities' element={<h1>Activities</h1>} />
+          <Route path='/login' element={<h1>Login</h1>} />
+          <Route path='/myRoutines' element={<h1>MyRoutines</h1>} />
+        </Routes>
+      </div>
+    </div>
   )
 }
 
