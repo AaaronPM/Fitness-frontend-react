018a83bb0a9124c16c75e82f7982a82efdd6121e
KyleBPerez <star_wars51389@yahoo.com>
2022-03-02 15:36:47 -0800

    added RoutineCards.jsx component to handle the routine card rendering

--- src/components/Routines.jsx
+++ src/components/Routines.jsx
@@ -1,45 +1,11 @@
-import Card from 'react-bootstrap/Card'
+import RoutineCards from './RoutineCards'
+
 export default function Routines({ routines }) {
   if (!routines) return <h1>Loading</h1>
 
   return (
     <div className='d-flex gap-3'>
-      {routines &&
-        routines.map((routine, idx) => {
-          console.log('routine -->', routine)
-          return (
-            <Card
-              key={routine.id}
-              style={{ width: '18em' }}
-              className='d-flex flex-column'
-            >
-              <Card.Header>{routine.name}</Card.Header>
-              <Card.Body>
-                <Card.Title>Creator: {routine.creatorName}</Card.Title>
-                <Card.Text className='p-0 m-0'>Goal:</Card.Text>
-                <Card.Text>{routine.goal}</Card.Text>
-                <Card.Text className='p-0 m-0'>Related Activities:</Card.Text>
-                {routine.activities.length > 0 ? (
-                  routine.activities.map((activity) => {
-                    return (
-                      <div key={activity.id} className='ps-3'>
-                        <ul className='list-unstyled p-0 m-0'>
-                          <li>{activity.name}</li>
-                        </ul>
-                      </div>
-                    )
-                  })
-                ) : (
-                  <div>
-                    <span className='ps-3'>
-                      No activites linked to this routine
-                    </span>
-                  </div>
-                )}
-              </Card.Body>
-            </Card>
-          )
-        })}
+      <RoutineCards routines={routines} />
     </div>
   )
 }
