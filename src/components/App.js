b4049ce5a3822608921906c7713082cecaff1fb1
KyleBPerez <star_wars51389@yahoo.com>
2022-03-04 21:38:12 -0800

    merge

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect, useState } from 'react'
 import { Routes, Route, useNavigate } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -11,62 +10,49 @@ import CreateRoutine from './CreateRoutine'
 import MyRoutines from './MyRoutines'
 import SingleRoutineCard from './SingleRoutineCard'
 import { fetchActivities, fetchRoutines, getUser } from '../api'
-=======
-import React, { useEffect, useState } from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import Header from "./Header";
-import Routines from "./Routines";
-import Login from "./Login";
-import Activities from "./Activities";
-import CreateRoutine from "./CreateRoutine";
-import MyRoutines from "./MyRoutines";
-import { fetchActivities, fetchRoutines, getUser } from "../api";
-import Home from "./Home";
->>>>>>> ec00b55d63cc92f015e5581cccbddfa09ee11881
+import Home from './Home'
 
 function App() {
-  const [user, setUser] = useState({});
-  const [routines, setRoutines] = useState([]);
-  const [activities, setActivities] = useState([]);
-  const [token, setToken] = useState("");
-  const navigate = useNavigate();
+  const [user, setUser] = useState({})
+  const [routines, setRoutines] = useState([])
+  const [activities, setActivities] = useState([])
+  const [token, setToken] = useState('')
+  const navigate = useNavigate()
 
   useEffect(() => {
-    fetchRoutines().then((allRoutines) => setRoutines(allRoutines));
-    fetchActivities().then((allActivities) => setActivities(allActivities));
-  }, []);
+    fetchRoutines().then((allRoutines) => setRoutines(allRoutines))
+    fetchActivities().then((allActivities) => setActivities(allActivities))
+  }, [])
 
   useEffect(() => {
-    if (localStorage.getItem("token")) {
-      setToken(localStorage.getItem("token"));
+    if (localStorage.getItem('token')) {
+      setToken(localStorage.getItem('token'))
     }
-  }, []);
+  }, [])
 
   useEffect(() => {
-    userHandler(token);
-  }, [token]);
+    userHandler(token)
+  }, [token])
 
   const userHandler = async (token) => {
     if (token) {
-      const user = await getUser(token);
-      setUser(user);
+      const user = await getUser(token)
+      setUser(user)
     }
-  };
+  }
 
   return (
-    <div className="App">
+    <div className='App'>
       <Header token={token} setToken={setToken} setUser={setUser} />
-      <div className="content-container d-flex justify-content-center mb-5">
+      <div className='content-container d-flex justify-content-center mb-5'>
         <Routes>
-          <Route path="/" element={<Home  />} />
+          <Route path='/' element={<Home />} />
           <Route
-            path="/routines"
+            path='/routines'
             element={<Routines token={token} routines={routines} />}
           />
           <Route
-            path="/routines/create-routine"
+            path='/routines/create-routine'
             element={
               <CreateRoutine
                 routines={routines}
@@ -76,12 +62,12 @@ function App() {
             }
           />
           <Route
-            path="/activities"
+            path='/activities'
             element={<Activities user={user} activities={activities} />}
           />
-          <Route path="/login" element={<Login setToken={setToken} />} />
+          <Route path='/login' element={<Login setToken={setToken} />} />
           <Route
-            path="/myRoutines"
+            path='/myRoutines'
             element={<MyRoutines routines={routines} user={user} />}
           />
           <Route
@@ -96,10 +82,9 @@ function App() {
             }
           />
         </Routes>
-       
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
