af2235238ba1d8b46527f0e8ab7b9cbb611a7f43
KyleBPerez <star_wars51389@yahoo.com>
2022-03-07 16:10:24 -0800

    made Home.js Buttons random color on page entry

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,11 +4,11 @@ import LinkContainer from 'react-router-bootstrap/LinkContainer'
 const RNGColor = () => {
   let RNG = Math.ceil(Math.random() * 5)
   const colorPairs = {
-    1: 'text-primary',
-    2: 'text-success',
-    3: 'text-danger',
-    4: 'text-warning',
-    5: 'text-info',
+    1: 'primary',
+    2: 'success',
+    3: 'danger',
+    4: 'warning',
+    5: 'info',
   }
   return `${colorPairs[RNG]}`
 }
@@ -25,7 +25,7 @@ export default function Home({ user }) {
           <hr className='my-4' />
           <p>Register or Login to start sharing your great WORKOUTS!</p>
           <LinkContainer to='/login'>
-            <Button variant='outline-success' className='btn-lg'>
+            <Button variant={`${RNGColor()}`} className='btn-lg'>
               Login / Register
             </Button>
           </LinkContainer>
@@ -34,18 +34,27 @@ export default function Home({ user }) {
         <div className='bg-light text-secondary p-5 rounded mt-5 shadow-lg'>
           <h1 className='display-4 mb-5 text-decoration-underline'>
             Welcome back,
-            <span className={`${RNGColor()} fw-bolder`}>{user.username}</span>!
+            <span className={`text-${RNGColor()} fw-bolder`}>
+              {user.username}
+            </span>
+            !
           </h1>
           <p className='lead'>Go Checkout all the great Routines!!!</p>
           <LinkContainer to='/routines'>
-            <Button variant='outline-secondary' className='btn-lg btn-block'>
+            <Button
+              variant={`outline-${RNGColor()}`}
+              className={`btn-lg btn-block`}
+            >
               Routines
             </Button>
           </LinkContainer>
           <hr className='my-4' />
           <p>Register or Login to start sharing your great WORKOUTS!</p>
           <LinkContainer to='/myRoutines'>
-            <Button variant='outline-primary' className='btn-lg btn-block'>
+            <Button
+              variant={`outline-${RNGColor()}`}
+              className='btn-lg btn-block'
+            >
               My Routines!!
             </Button>
           </LinkContainer>
